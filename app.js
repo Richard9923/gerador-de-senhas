8aea8aa8e9a62ce9295fae66ff5249f168447736
Richard <programmingislife23@gmail.com>
2024-02-21 15:36:20 -0300

    criando conceitos basicos para o projeto

---
+++ app.js
@@ -0,0 +1,11 @@
+const letras = ["a", "b", "c", "A", "B", "C"];
+const numeros = ["1", "2", "3", "4"];
+const simbolos = ["!", "@", "#", "$", "%"];
+
+quantidadeDeLetras = prompt("Quantidade de letras você quer na sua senha? ");
+quantidadeDeNumeros = parseFloat(
+  prompt("Quantidade de números que você quer na sua senha?")
+);
+quantidadeDeSimbolos = prompt(
+  "Quantidade de simbolos que você quer na sua senha?"
+);
