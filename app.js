cfd924a22e036c52e5766728ea7910f048b14c53
Richard <programmingislife23@gmail.com>
2024-02-21 16:04:14 -0300

    projeto finalizado e funcionando perfeitamente

--- app.js
+++ app.js
@@ -2,10 +2,34 @@ const letras = ["a", "b", "c", "A", "B", "C"];
 const numeros = ["1", "2", "3", "4"];
 const simbolos = ["!", "@", "#", "$", "%"];
 
-quantidadeDeLetras = prompt("Quantidade de letras você quer na sua senha? ");
-quantidadeDeNumeros = parseFloat(
+quantidadeDeLetras = parseInt(
+  prompt("Quantidade de letras você quer na sua senha? ")
+);
+quantidadeDeNumeros = parseInt(
   prompt("Quantidade de números que você quer na sua senha?")
 );
-quantidadeDeSimbolos = prompt(
-  "Quantidade de simbolos que você quer na sua senha?"
+quantidadeDeSimbolos = parseInt(
+  prompt("Quantidade de simbolos que você quer na sua senha?")
 );
+
+senha = [];
+for (i = 0; i < quantidadeDeLetras; i++) {
+  let randomNumber = Math.floor(Math.random() * letras.length);
+  const randomLetra = letras[randomNumber];
+  senha.push(randomLetra);
+}
+for (i = 0; i < quantidadeDeNumeros; i++) {
+  let randomNumber = Math.floor(Math.random() * numeros.length);
+  const numeroAleotorio = numeros[randomNumber];
+  senha.push(numeroAleotorio);
+}
+for (i = 0; i < quantidadeDeSimbolos; i++) {
+  let randomNumber = Math.floor(Math.random() * simbolos.length);
+  const simboloAleotorio = simbolos[randomNumber];
+  senha.push(simboloAleotorio);
+}
+console.log(senha);
+const shuffledSenha = senha.sort((a, b) => 0.5 - Math.random());
+let senhaAtualizada = shuffledSenha.toString();
+const senhaFinal = senhaAtualizada.replace(/,/g, "");
+console.log(`Senha gerada: ${senhaFinal}`);
